cf87fffc954e3691a4b36c24fcaed6aed3a61717
高木　大介 <takagi-d@iij.ad.jp>
2024-11-14 13:05:27 +0900

    SpringInitializrで作成した資材を登録。

---
+++ src/main/java/com/sb/sblib/SblibApplication.java
@@ -0,0 +1,13 @@
+package com.sb.sblib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SblibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SblibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sb/sblib/SblibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.sblib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SblibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
